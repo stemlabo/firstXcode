41f591c8889a2c1dd5bc2787efdab94a912270d0
kyosukenakashima <regolith.mail@gmail.com>
2019-04-04 02:00:04 +0900

    4月3日3commit

---
+++ NilAndOptional.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+var a:  Int = 123
+var b:  Double = 123
+
+var c:  Int?
+c = 456
+c = nil
+
+c = 5
+var d: Int? = 7
+//let e = c + d
+let f = c! + d!
