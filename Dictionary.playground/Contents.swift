8ddc71abca9af67144febc429af3ebc20895e310
kyosukenakashima <regolith.mail@gmail.com>
2019-04-04 03:23:37 +0900

    4月3日8commit

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let a = ["Taro": 1985, "Hanako": 1986]
+
+let b = a["Taro"]
+let c = a["Hanako"]
+
+
+var d: [String: Int] = ["Taro": 1985, "Hanako": 1986]
+d["Jiro"] = 1988
+d
+d["Taro"] = 1984
+d.removeValue(forKey: "Hanako")
+d
