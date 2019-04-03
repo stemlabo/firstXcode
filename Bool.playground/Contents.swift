dba25471b0705f706ff2016e95b7ab5aeb50c703
kyosukenakashima <regolith.mail@gmail.com>
2019-04-04 02:27:29 +0900

    4月3日5commit

---
+++ Bool.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let a = 3
+let b = 5
+
+let c = a == b
+let d = a != b
+let e = a > b
+let f = a < b
+
+let g = a >= 3
+let h = a <= 2
+
+let i = a > 2 && b < 7
+let j = a > 2 || b < 2
