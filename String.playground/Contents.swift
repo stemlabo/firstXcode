8364d70a689da7725c0148c41ca379d7f0d0cd35
kyosukenakashima <regolith.mail@gmail.com>
2019-04-04 02:18:48 +0900

    4月3日4commit

---
+++ String.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+var str: String = "Hello, playground"
+
+let a = "Hellow "
+let b = "World!"
+
+let c = a + b
+print(c)
+
+let d = "私は"
+let e = "歳です"
+let f = 20
+
+let g = d + "\(f)" + e
+print(g)
+
