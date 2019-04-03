e168080efdc28b9bb4dc34377729f769ddc431eb
kyosukenakashima <regolith.mail@gmail.com>
2019-04-04 02:42:32 +0900

    4月3日6commit

---
+++ Branch.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let a = true
+if a {
+    print("hello!")
+}else{
+    print("Hi!")
+
+}
+
+let time = 21
+
+if time < 12 {
+    print("Good morning")
+}else if time < 18 {
+    print("Good afternoon!")
+}else{
+    print("Good night!")
+    
+}
