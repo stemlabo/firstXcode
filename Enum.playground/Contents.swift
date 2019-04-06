c33d07c68406aa283ffcd329a46a236e2e192daf
kyosukenakashima <regolith.mail@gmail.com>
2019-04-06 15:19:12 +0900

    30-列挙型

---
+++ Enum.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+enum Signal {
+    case Blue
+    case Yellow
+    case Red
+}
+
+var sig: Signal
+sig = Signal.Blue
+sig = .Blue
+
+switch sig{
+case .Blue:
+    print("Go!")
+case .Yellow:
+    print("Caution!")
+case .Red:
+    print("Stop")
+    
+
