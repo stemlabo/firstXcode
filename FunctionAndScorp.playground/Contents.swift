2579b598611ee01fc1629630220f9c87200a4deb
kyosukenakashima <regolith.mail@gmail.com>
2019-04-06 12:36:10 +0900

    22-関数とスコープ

---
+++ FunctionAndScorp.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+func a() {
+    print("Hellow!")
+}
+a()
+
+let b = 123
+func c() {
+    if b == 123 {
+        print("Hi!")
+    }
+}
+c()
+
+func d() {
+    let e = 456
+    print(e)
+}
+d()
+print(e)
